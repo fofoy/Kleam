cb7409a02b2471b69807506258040dedaf2eb1e9
Florian Quiblier <florian.quiblier@gmail.com>
2013-05-30 11:09:32 +0200

    Steam Connection

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -10,7 +10,39 @@ class App_controller{
     }
  
     function home(){
-        echo Views::instance()->render('index.html');
+        switch(F3::get('VERB')){
+            case 'GET':
+                if(F3::get('SESSION.userId')){
+                    F3::reroute('/dashboard');
+                } else {
+                    echo Views::instance()->render('index.html');
+                }
+            break;
+            case 'POST':
+                require('app/Helpers/Library/openid.php');
+                # Change 'localhost' to your domain name.
+                $openid = new LightOpenID('localhost');
+                if(!$openid->mode) {
+                    $openid->identity = 'http://steamcommunity.com/openid/';
+                    header('Location: ' . $openid->authUrl());
+                } elseif($openid->mode == 'cancel') {
+                    echo 'User has canceled authentication!';
+                } else {
+                    echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
+                    F3::set('SESSION.userId',$openid->identity);
+                    F3::reroute('/dashboard');
+                }
+            break;
+        }
+    }
+
+    function dashboard(){
+        if(F3::get('SESSION.userId')){
+            echo F3::get('SESSION.userId');
+            echo Views::instance()->render('dashboard.html');
+        } else {
+            F3::reroute('/');
+        }
     }
 
     function __destruct(){
