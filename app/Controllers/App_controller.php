00065c9efadb71aa1bbfd7af69e17a663665005d
Florian Quiblier <florian.quiblier@gmail.com>
2013-06-10 09:26:29 +0200

    Dashboard

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -1,48 +1,48 @@
 <?php
 class App_controller{
 
-    private $basket;
-
     function __construct(){
-        $this->basket=new \Basket;
-        $count=$this->basket->count();
-        F3::set('count',$count);
+    
     }
  
     function home(){
         switch(F3::get('VERB')){
             case 'GET':
-                if(F3::get('SESSION.userId')){
+                if(F3::get('SESSION.id')){
                     F3::reroute('/dashboard');
                 } else {
                     echo Views::instance()->render('index.html');
                 }
             break;
             case 'POST':
-                require('app/Helpers/Library/openid.php');
-                # Change 'localhost' to your domain name.
-                $openid = new LightOpenID('localhost');
-                if(!$openid->mode) {
-                    $openid->identity = 'http://steamcommunity.com/openid/';
-                    header('Location: ' . $openid->authUrl());
-                } elseif($openid->mode == 'cancel') {
-                    echo 'User has canceled authentication!';
-                } else {
-                    echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
-                    F3::set('SESSION.userId',$openid->identity);
-                    F3::reroute('/dashboard');
-                }
             break;
         }
     }
 
-    function dashboard(){
-        if(F3::get('SESSION.userId')){
-            echo F3::get('SESSION.userId');
-            echo Views::instance()->render('dashboard.html');
-        } else {
-            F3::reroute('/');
+    function login(){
+        require('app/Helpers/Library/openid.php');
+        $openid = new LightOpenID('localhost');
+        $openid->identity = 'http://steamcommunity.com/openid/';
+        header('Location: ' . $openid->authUrl());
+        if ($openid->mode) {
+
+            if($openid->validate()){
+                echo $openid->validate() ? 'Logged in.' : 'Failed';
+                F3::set('infos',$openid);
+                F3::reroute('/dashboard');
+            }
+            echo $openid->validate() ? 'Logged in.' : 'Failed';
         }
+    }
+
+    function done(){
+        $openid=new OpenID;
+        echo $openid->verified()?'Success':'Failure';
+    }
+
+    function dashboard(){
+        //require('app/Helpers/Library/steam-condenser.php');
+        echo Views::instance()->render('dashboard.html');
     }
 
     function __destruct(){
