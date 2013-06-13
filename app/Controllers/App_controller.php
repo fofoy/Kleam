34bedc35636c1d51989f6af74f5984fcef82a654
Florian Quiblier <florian.quiblier@gmail.com>
2013-06-13 08:29:40 +0200

    Datas

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -1,8 +1,10 @@
 <?php
 class App_controller{
 
+    public $openid_infos;
+
     function __construct(){
-    
+
     }
  
     function home(){
@@ -25,10 +27,11 @@ class App_controller{
         $openid->identity = 'http://steamcommunity.com/openid/';
         header('Location: ' . $openid->authUrl());
         if ($openid->mode) {
-
             if($openid->validate()){
                 echo $openid->validate() ? 'Logged in.' : 'Failed';
-                F3::set('infos',$openid);
+                $lol = $openid->getAttributes();
+                //F3::set('SESSION.openid',$openid);
+                F3::set('SESSION.lolo',$lol);
                 F3::reroute('/dashboard');
             }
             echo $openid->validate() ? 'Logged in.' : 'Failed';
@@ -44,7 +47,14 @@ class App_controller{
     }
 
     function dashboard(){
-        //require('app/Helpers/Library/steam-condenser.php');
+        require('app/Helpers/Library/steam-condenser.php');
+        $id = SteamId::create('76561197966166493');
+        $stats = $id->getGames();
+        //$achievements = $stats->getAchievements();
+        F3::set('id',F3::get('SESSION.id'));
+        F3::set('lol',$stats);
+        F3::set('count',count($stats));
+        F3::set('json',json_encode($stats));
         echo Views::instance()->render('dashboard.html');
     }
 
