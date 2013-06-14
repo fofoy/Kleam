8fb4f3f7b2c65314be24d98502d4eb4367a484a9
Florian Quiblier <florian.quiblier@gmail.com>
2013-06-14 13:02:32 +0200

    Json improvements

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -2,6 +2,7 @@
 class App_controller{
 
     public $openid_infos;
+    public $profile;
 
     function __construct(){
 
@@ -30,6 +31,7 @@ class App_controller{
             if($openid->validate()){
                 echo $openid->validate() ? 'Logged in.' : 'Failed';
                 $lol = $openid->getAttributes();
+                //$ajax['pictures']=array_map(function($item){return array('image'=>$item->src);},$pictures);
                 //F3::set('SESSION.openid',$openid);
                 F3::set('SESSION.lolo',$lol);
                 F3::reroute('/dashboard');
@@ -51,13 +53,67 @@ class App_controller{
 
     function dashboard(){
         require('app/Helpers/Library/steam-condenser.php');
-        $id = SteamId::create('76561197966166493');
-        $stats = $id->getGames();
-        //$achievements = $stats->getAchievements();
+        $steamId = SteamId::create('76561197964139068');
+        $gamesArray = $steamId->getGames();
+        $friendsArray = $steamId->getFriends();
         F3::set('id',F3::get('SESSION.id'));
-        F3::set('lol',$stats);
-        F3::set('count',count($stats));
-        F3::set('json',json_encode($stats));
+        $ajax['infos']['nickname']=$steamId->getNickname();
+        $ajax['infos']['avatar']=stripslashes($steamId->getFullAvatarUrl());
+        $ajax['games']=array_map(function($item){
+            $steamId = SteamId::create('76561197964139068');
+            if($item->shortName=='alienswarm'||$item->shortName=='defensegrid:awakening'||$item->shortName=='l4d'||$item->shortName=='l4d2'||$item->shortName=='portal2'||$item->shortName=='tf2'){
+                return array(
+                    'appId'=>$item->appId,
+                    'gameName'=>$item->name,
+                    'shortName'=>$item->shortName,
+                    'playedTime'=>$steamId->getTotalPlaytime($item->appId),
+                    'achievements'=>$steamId->getGameStats($item->shortName)->getAchievementsDone()
+                );
+            }else{
+                return array(
+                    'appId'=>$item->appId,
+                    'gameName'=>$item->name,
+                    'shortName'=>$item->shortName,
+                    'playedTime'=>$steamId->getTotalPlaytime($item->appId),
+                    'achievements'=>0
+                );
+            }
+        },$gamesArray);
+        //$ajax['games']=array_map("array_pop",$ajax['games']);
+        /*$ajax['friends']=array_map(function($item){
+            $steamId = SteamId::create($item->steamId64);
+            return array(
+                'infos'=>array(
+                    'steamId'=>$item->steamId64,
+                    'nickname'=>$steamId->getNickName(),
+                    'avatar'=>$steamId->getIconAvatarUrl()
+                )
+            );
+        },$friendsArray);*/
+        /*$ajax['friends']=array_map(function($item1){
+            $steamId=SteamId::create($item1->steamId64);
+            return array(
+                'steamId'=>$steamId,
+                'nickname'=>$steamId->getNickName(),
+                //'avatar'=>stripslashes($steamId->getFullAvatarUrl())
+                $ajax['friends']['games']=array_map(function($item2){
+                    $steamId=SteamId::create($item1->steamId64);
+                    return array(
+                        'appId'=>$item2->appId,
+                        'gameName'=>$item2->name,
+                        'shortName'=>$item2->shortName,
+                        'playedTime'=>$steamId->getTotalPlaytime($item2->appId),
+                        'achievements'=>0
+                    );
+                },$steamId);
+        )},$friendsArray);*/
+        $json = json_encode($ajax);
+
+        $fp = fopen('results.json', 'w');
+        fwrite($fp, json_encode($ajax));
+        fclose($fp);
+
+        F3::set('json',$json);
         echo Views::instance()->render('dashboard.html');
     }
 
